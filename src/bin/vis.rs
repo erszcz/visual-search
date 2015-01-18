3851737bb097df1d534080c880d49c3e647e46fc
Radek Szymczyszyn <lavrin@gmail.com>
2015-01-18 16:58:51 +0100

    Make vis display 4 colours in a round-robin fashion

--- src/bin/vis.rs
+++ src/bin/vis.rs
@@ -7,15 +7,61 @@ extern crate sdl2_window;
 extern crate opengl_graphics;
 extern crate clock_ticks;
 
-use std::cell::RefCell;
+use frame_counter::{ FrameCounter, FrameUpdate };
 use opengl_graphics::{ Gl,Texture };
 use sdl2_window::Sdl2Window;
+use std::cell::RefCell;
+use std::io;
+
+macro_rules! errorln {
+    ($fmt:expr) => {
+        (writeln![&mut io::stdio::stderr(), $fmt]).ok().expect("log failed")
+    };
+    ($fmt:expr, $($msg:tt)*) => {
+        (writeln![&mut io::stdio::stderr(), $fmt, $($msg)*]).ok().expect("log failed")
+    };
+}
+
+mod frame_counter;
 
 fn main() {
-    let start = ( clock_ticks::precise_time_ns() / 1000 / 1000 ) as usize;
-    println!("start  : {}ms", start);
-    std::io::timer::sleep(std::time::Duration::milliseconds(100));
-    let end = ( clock_ticks::precise_time_ns() / 1000 / 1000 ) as usize;
-    println!("end    : {}ms", end);
-    println!("elapsed: {}ms", end - start);
+    let mut fc = FrameCounter::from_fps(2);
+    let opengl = shader_version::OpenGL::_3_2;
+    // TODO: determine based on input map
+    let (width, height) = (300, 300);
+    let window = Sdl2Window::new(
+        opengl,
+        event::WindowSettings {
+            title: "Graph Search".to_string(),
+            size: [width, height],
+            //fullscreen: true,
+            fullscreen: false,
+            exit_on_esc: true,
+            samples: 0
+        }
+    );
+    let mut image = image::ImageBuffer::new(width, height);
+    let mut frame = 0;
+    let mut texture = Texture::from_image(&image);
+    let ref mut gl = Gl::new(opengl);
+    let window = RefCell::new(window);
+    for e in event::events(&window) {
+        use event::{ RenderEvent };
+        if let Some(args) = e.render_args() {
+            let color = match frame {
+                0 => [1.0, 0.0, 0.0, 1.0],
+                1 => [0.0, 1.0, 0.0, 1.0],
+                2 => [0.0, 0.0, 1.0, 1.0],
+                _ => [1.0, 1.0, 0.0, 1.0]
+            };
+            if let FrameUpdate::NewFrame{skipped_frames, ..} = fc.update() {
+                errorln!("new frame: skipped={:?}", skipped_frames);
+                frame = (frame + skipped_frames) % 4;
+            }
+            gl.draw([0, 0, args.width as i32, args.height as i32], |c, gl| {
+                graphics::clear(color, gl);
+                graphics::image(&texture, &c, gl);
+            });
+        };
+    }
 }
